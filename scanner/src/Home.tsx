7bec8b841561392e8209982a2aa29797af808b1b
Milos Zivlak <zivlakmilos@gmail.com>
2025-12-21 17:06:40 +0100

    Home screen layout

--- scanner/src/Home.tsx
+++ scanner/src/Home.tsx
@@ -2,10 +2,12 @@ import type { Component } from 'solid-js';
 
 const Home: Component = () => {
   return (
-    <>
-      <button class="btn btn-primary">Primary</button>
-      <button class="btn btn-primary">Primary</button>
-    </>
+    <div class="flex items-center justify-center h-screen">
+      <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <button class="btn btn-neutral">Scan QR</button>
+        <button class="btn btn-neutral mt-4">Maunal Input</button>
+      </fieldset>
+    </div>
   );
 };
 
--- scanner/src/Login.tsx
+++ scanner/src/Login.tsx
@@ -1,4 +1,3 @@
-
 import type { Component } from 'solid-js';
 
 const Login: Component = () => {
