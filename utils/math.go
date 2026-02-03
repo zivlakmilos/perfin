3bd670504ab5cfc2264e00ecab2212e85dba8010
Milos Zivlak <zivlakmilos@gmail.com>
2026-02-04 00:35:32 +0100

    Fix typeo

--- utils/math.go
+++ utils/math.go
@@ -1,6 +1,6 @@
 package utils
 
-func ComareFloat4(a, b float64) bool {
+func CompareFloat4(a, b float64) bool {
 	precision := float64(10000)
 	return int64(a*precision+0.5) == int64(b*precision+0.5)
 }
